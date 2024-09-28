e23b91d92dacfdd5c9b9c63130214948099dfd92
WorldOfEphemeral <154613962+WorldOfEphemeral@users.noreply.github.com>
2024-09-28 20:27:14 +0800

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -28,15 +27,39 @@ pub enum Command {
     Append(usize),
 }
 
-mod my_module {
+pub mod my_module {
+    use std::ops::Add;
     use super::Command;
 
+    fn append(mut s:String, x:usize)->String{
+        for _i in 0..x{
+            s.push_str("bar")
+        }
+        s
+    }
+
+    fn trim(s:String)->String{
+        let res = s.trim();
+        res.to_string()
+    }
+
+    fn uppercase(s:String)->String{
+        s.to_uppercase()
+    }
+
+
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let res = match command {
+                Command::Append(x) => self::append(string.clone(), *x),
+                Command::Trim => self::trim(string.clone()),
+                Command::Uppercase => self::uppercase(string.clone()),
+            };
+            output.push(res)
         }
         output
     }
@@ -45,7 +68,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
